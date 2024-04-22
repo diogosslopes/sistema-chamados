be2f113bb1c4cd65f5fd80da86a08c341e61ab50
Diogo Sousa Silva Lopes <67078434+diogosslopes@users.noreply.github.com>
2024-04-22 14:08:40 -0300

    Update auth.js

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -85,8 +85,8 @@ function AuthProvider({ children }) {
     }
 
 
-    const baseURL = "https://servidor-teste-mauve.vercel.app"
-    // const baseURL = "http://localhost:3002"
+    const baseURL = "https://server-three-navy.vercel.app"
+    // const baseURL = "http://localhost:3001"
 
 
     async function registerUser(value) {
